7aa8cd6be53565e641315e5104060adf31f22b37
oumaima tiguint <oumaimatiguint@gmail.com>
2018-11-08 12:30:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -24,9 +25,21 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.get("/api/whoami", function (req, res) {
+  var ipAddress = req.ip;
+  var trimedIpAddress = ipAddress.replace(/[A-Za-z]|:/g, '');
+  
+  var language = req.headers["accept-language"];
+  var trimedLanguage = language.slice(0, language.indexOf(','));
+  
+  var software = req.headers["user-agent"];
+  var start = software.indexOf('\(');
+  var end= software.indexOf('\)');
+  var trimedSoftware = software.slice(start + 1, end);
+  
+  res.json({
+    "ipadress": trimedIpAddress,
+    "language": trimedLanguage,
+    "software": trimedSoftware
+    });
 });
