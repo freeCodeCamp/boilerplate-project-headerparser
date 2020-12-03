d9d31932dbf10cc735fe43e011b1e3206ed377b6
pmayur <thisispatil@gmail.com>
2020-12-03 11:31:58 +0530

    Add endpoint for header parsing

--- server.js
+++ server.js
@@ -18,15 +18,20 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/whoami", function(req,res) {
+  res.send({
+    "ipaddress": req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+    "language": req.headers['accept-language'],
+    "software": req.headers['user-agent']
+  })
+})
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
