2001c9f869fc1de06cbab695db00f3bf14320096
Lasse Jørgensen <28780271+lasjorg@users.noreply.github.com>
2022-07-15 23:10:54 +0200

    fix(config/deps): update and a little cleanup (#28)
    * fix(config/deps): update and a little cleanup
    
    * chore(format): prettier, add .prettierignore
    
    * fix(config): add .gitattributes

--- index.js
+++ index.js
@@ -7,25 +7,22 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
