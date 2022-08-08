cc97f589133a0b1536e4eb183a417734aab925d9
Mr Skywalker <kamogelooliphant140@gmail.com>
2022-08-08 11:12:51 +0200

    change

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  res.json({ ipaddress: req.connection.remoteAddress, 
+            language: req.headers["accept-language"], 
+            software:req.headers["user-agent"] });
 });
 
 // listen for requests :)
