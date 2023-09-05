0aa689ffd18c80c214185641e5133529a432ca8a
dezaerus <adtow187@gmail.com>
2023-09-05 17:58:43 +0300

    Finished task

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+app.get('/api/whoami', function (req, res) {
+  const ipAddress = req.ip;
+  const language = req.headers['accept-language']; 
+  const software = req.headers['user-agent'];
+
+  res.json({ ipaddress: ipAddress, language: language, software: software });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
