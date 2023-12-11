27d457774014f3a1d25c52d6d6d62c6b1ddaaf33
Rajesh Jonnalagadda <rajeshjonnalagadda48@gmail.com>
2023-12-11 23:06:07 +0530

    added whoami endpoint

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  let headers = req.headers
+  res.json({ 
+    ipaddress: req.ip,
+    language: headers['accept-language'] || '',
+    software: headers['user-agent'] || ''
+   });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
